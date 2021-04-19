6aa4f5674a792fc8513cd2abe57749f48b773cf1
annamariacutolo <52924438+annamariacutolo@users.noreply.github.com>
2021-04-19 18:29:24 +0100

    fetch recipe api

--- src/App.js
+++ src/App.js
@@ -2,22 +2,57 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './components/Home'
 import Search from './components/Search'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 const App = () => {
   const [search, setSearch] = useState(false)
 
-  return (
-    <Router>
-    <div className="App">
-      <Header />
-      {/*{search === false ? <Home /> : <Search />}*/}
-      <Route path='/' exact component={Home} />
-      <Route path='/search' exact component={Search} />
-    </div>
-    </Router>
-  );
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [recipes, setRecipes] = useState([])
+  
+  useEffect(() => {
+    fetch('/recipes')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true)
+          setRecipes(result)
+        },
+        (error) => {
+          setIsLoaded(true)
+          setError(error)
+        }
+      )
+  }, [])
+  
+  if (error) {
+    return <div>Error: {error.message}</div>
+  } else if (!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>
+            {recipe.name}
+          </li>
+        ))}
+      </ul>
+    )
+  }
+
+  // return (
+  //   <Router>
+  //   <div className="App">
+  //     <Header />
+  //     {/*{search === false ? <Home /> : <Search />}*/}
+  //     <Route path='/' exact component={Home} />
+  //     <Route path='/search' exact component={Search} />
+  //   </div>
+  //   </Router>
+  // );
 }
 
 export default App;
