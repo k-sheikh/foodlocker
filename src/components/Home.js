93b6740e5ceb981932f80e2820b444f47db301a1
annamariacutolo <52924438+annamariacutolo@users.noreply.github.com>
2021-04-20 19:27:27 +0100

    displaying 5 top rated recipeitems

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,45 +1,8 @@
-import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import RecipeCategory from './RecipeCategory'
 import Button from './Button';
 
 const Home = () => {
-    // const [error, setError] = useState(null)
-    // const [isLoaded, setIsLoaded] = useState(false)
-    const [recipes, setRecipes] = useState([])
-    
-    useEffect(() => {
-      fetch('/recipes')
-        .then(res => res.json())
-        .then(
-          (result) => {
-            // setIsLoaded(true)
-            setRecipes(result)
-          }
-        //   (error) => {
-        //     setIsLoaded(true)
-        //     setError(error)
-        //   }
-        )
-    }, [])
-    
-    // if (error) {
-    //   return <div>Error: {error.message}</div>
-    // } else if (!isLoaded) {
-    //   return <div>Loading...</div>
-    // } else {
-          
-    const ids = recipes.filter(recipe => recipe.rating === 5)
-    console.log('IDs', ids)
-    
-    const topRated = []
-    for (let i=0; i<5; i++) {
-      const randomRecipe = ids[Math.floor(Math.random() * ids.length)]
-      console.log(randomRecipe)
-      topRated.push(randomRecipe)
-      console.log('top-rated', topRated)  
-    }
-
     const onClick = () => {
         window.location = '/search'
         console.log('go to search');
@@ -49,7 +12,6 @@ const Home = () => {
         <div>
             <Button text="search recipes" onClick={onClick}/>
             <RecipeCategory text='Top-rated'/>
-            <ul>{topRated.map(recipe => <li>{recipe.name}</li>)}</ul>
         </div>
     )
 }
--- src/components/RecipeCategory.js
+++ src/components/RecipeCategory.js
@@ -1,11 +1,48 @@
+import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import RecipeItem from './RecipeItem'
 
 const RecipeCategory = ({ text }) => {
+    // const [error, setError] = useState(null)
+    // const [isLoaded, setIsLoaded] = useState(false)
+    const [recipes, setRecipes] = useState([])
+    
+    useEffect(() => {
+      fetch('/recipes')
+        .then(res => res.json())
+        .then(
+          (result) => {
+            // setIsLoaded(true)
+            setRecipes(result)
+          }
+        //   (error) => {
+        //     setIsLoaded(true)
+        //     setError(error)
+        //   }
+        )
+    }, [])
+    
+    // if (error) {
+    //   return <div>Error: {error.message}</div>
+    // } else if (!isLoaded) {
+    //   return <div>Loading...</div>
+    // } else {
+          
+    const ids = recipes.filter(recipe => recipe.rating === 5)
+    console.log('IDs', ids)
+    
+    const topRated = []
+    for (let i=0; i<5; i++) {
+      const randomRecipe = ids[Math.floor(Math.random() * ids.length)]
+      console.log(randomRecipe)
+      topRated.push(randomRecipe)
+    }
+    console.log('top-rated', topRated)
+
     return ( 
         <div>
             <h3>{text}</h3>
-            <RecipeItem name='burger' />
+            <p>{topRated.map(recipe => <RecipeItem name={recipe.name} rating={recipe.rating} />)}</p>
         </div>
     )
 }
