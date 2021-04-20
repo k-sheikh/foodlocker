4040e676629473d8e9a29d5281da7f2875658699
George Goodall <george.goodall@hotmail.co.uk>
2021-04-20 08:43:46 +0100

    add Footer component and add Footer to App.js

--- src/App.js
+++ src/App.js
@@ -1,58 +1,60 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Home from './components/Home'
-import Search from './components/Search'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import Search from './components/Search';
+import Footer from './components/Footer';
 import { useState, useEffect } from 'react'
 
 
 const App = () => {
-  const [search, setSearch] = useState(false)
+  // const [search, setSearch] = useState(false)
 
-  const [error, setError] = useState(null)
-  const [isLoaded, setIsLoaded] = useState(false)
-  const [recipes, setRecipes] = useState([])
+  // const [error, setError] = useState(null)
+  // const [isLoaded, setIsLoaded] = useState(false)
+  // const [recipes, setRecipes] = useState([])
   
-  useEffect(() => {
-    fetch('/recipes')
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true)
-          setRecipes(result)
-        },
-        (error) => {
-          setIsLoaded(true)
-          setError(error)
-        }
-      )
-  }, [])
+  // useEffect(() => {
+  //   fetch('/recipes')
+  //     .then(res => res.json())
+  //     .then(
+  //       (result) => {
+  //         setIsLoaded(true)
+  //         setRecipes(result)
+  //       },
+  //       (error) => {
+  //         setIsLoaded(true)
+  //         setError(error)
+  //       }
+  //     )
+  // }, [])
   
-  if (error) {
-    return <div>Error: {error.message}</div>
-  } else if (!isLoaded) {
-    return <div>Loading...</div>
-  } else {
-    return (
-      <ul>
-        {recipes.map((recipe) => (
-          <li key={recipe.id}>
-            {recipe.name}
-          </li>
-        ))}
-      </ul>
-    )
-  }
+  // if (error) {
+  //   return <div>Error: {error.message}</div>
+  // } else if (!isLoaded) {
+  //   return <div>Loading...</div>
+  // } else {
+  //   return (
+  //     <ul>
+  //       {recipes.map((recipe) => (
+  //         <li key={recipe.id}>
+  //           {recipe.name}
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   )
+  // }
 
-  // return (
-  //   <Router>
-  //   <div className="App">
-  //     <Header />
-  //     {/*{search === false ? <Home /> : <Search />}*/}
-  //     <Route path='/' exact component={Home} />
-  //     <Route path='/search' exact component={Search} />
-  //   </div>
-  //   </Router>
-  // );
+  return (
+    <Router>
+    <div className="App">
+      <Header />
+      {/*{search === false ? <Home /> : <Search />}*/}
+      <Route path='/' exact component={Home} />
+      <Route path='/search' exact component={Search} />
+      <Footer />
+    </div>
+    </Router>
+  );
 }
 
 export default App;
