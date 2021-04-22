19999b16e6b5dbead0a289fefbade14dd3ec874d
annamariacutolo <52924438+annamariacutolo@users.noreply.github.com>
2021-04-22 20:50:45 +0100

    fetch pics for each recipe on home page

--- src/components/RecipeCategory.js
+++ src/components/RecipeCategory.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import RecipeItem from './RecipeItem';
 
@@ -7,36 +7,12 @@ import RecipeItem from './RecipeItem';
 // we want to add it tou our app.js, (our global state), then we can pass them down as props.
 
 const RecipeCategory = ({ recipes, title }) => {
-    const [pics, setPics]  = useState([]);
-
-    useEffect(() => {
-        const getPics = async (query) => {
-            const response = await fetch(
-            `https://api.pexels.com/v1/search?query=${query}&per_page=1`,
-            {
-                method: "GET",
-                headers: {
-                    Accept: "application/json",
-                    Authorization: '563492ad6f9170000100000112c00782d84d44bbafd7828b5154b405'
-                }
-            }
-            );
-        const result = await response.json();
-        setPics(result.photos ?? []);
-        }
-
-        const query = (recipes.map(recipe => recipe.name))
-        getPics(query)
-        
-
-    }, [recipes]);
-    
     return (
         <div className='RecipeCategory'>
             <h2>{title}</h2>
             <div className='RecipeCategoryContent'>
                 {recipes.map((recipe) => (
-                    <RecipeItem key={ recipe.id } recipe={recipe} image={pics.map(pic => pic.src.small)} />
+                    <RecipeItem key={ recipe.id } recipe={recipe} />
                 ))}
             </div>
         </div>
